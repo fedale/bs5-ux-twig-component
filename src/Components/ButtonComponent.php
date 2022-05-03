ee71d5bbec543ce6445256e77594892c1d7d147e
Danilo Di Moia <ddimoia>
2022-05-03 09:09:49 -0400

    Button component

--- src/Components/ButtonComponent.php
+++ src/Components/ButtonComponent.php
@@ -28,8 +28,13 @@ class ButtonComponent
     /**
      * @var string
      */
-    public string $tagButton;
-
+    public string $tagType;
+    
+    /**
+     * @var string
+     */
+    public string $tagName;
+    
     /**
      * @var string
      */
@@ -45,6 +50,21 @@ class ButtonComponent
      */
     public $href;
 
+    /**
+     * @var bool
+     */
+    public string $outline;
+
+    /**
+     * @var string
+     */
+    public string $size;
+
+    /**
+     * @var bool|string
+     */
+    public $disabled;
+
     #[PreMount]
     public function preMount(array $data): array
     {
@@ -52,48 +72,90 @@ class ButtonComponent
         $resolver->setDefaults([
             'id' => 'button-' . \uniqid(),
             'type' => 'primary',
-            'tag' => 'button',
-            'tagButton' => 'button',
             'content' => 'my content',
-            'text' => 'dark',
-            'href' => '#'
+            'text' => 'light',
+            'href' => '#',
+            'tag' => 'button',
+            'outline' => false,
+            'size' => '',
+            'disabled' => false,
         ]);
-
-        $resolver->setAllowedTypes('href', ['string']); // It does not work in any combination
-
-        $resolver->setAllowedValues('type', ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark', 'link']);
-        $resolver->setAllowedValues('tag', ['link', 'button', 'input', 'submit', 'reset']);
-        $resolver->setAllowedValues('text', ['light', 'dark', '']);
-
-        $resolver->setNormalizer('type', function(Options $options, $value) {
-            return 'btn-' . $value;
-        });
         
-        $resolver->setNormalizer('tagButton', function(Options $options, $value) {
+        $resolver->setDefault('tagType', function (Options $options) {
+            $tag = $options['tag'];
+
+            switch ($tag) {
+                
+                case 'submit':    
+                    $value = 'submit';
+                    break;                
+                case 'input':
+                    $value = 'button';
+                    break;
+                case 'reset':
+                    $value = 'reset';
+                    break;
+                case 'button':
+                case 'link':
+                default:
+                    $value = '';
+                    break;
+            }
+
+            return $value;
+            }
+        );
+
+        $resolver->setDefault('tagName', function (Options $options) {
             $tag = $options['tag'];
 
             switch ($tag) {
                 case 'link':
                     $value = 'a';
                     break;
-                case 'button':
                 case 'submit':
+                case 'input':
                 case 'reset':
                     $value = 'input';
-                    break;
+                    break;                
                 case 'button':
                 default:
                     $value = 'button';
                     break;
             }
-
+            
             return $value;
+            }
+        );
+
+        $resolver->setAllowedTypes('href', ['string']); 
+        $resolver->setAllowedTypes('disabled', 'bool'); 
+
+        $resolver->setAllowedValues('type', ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark', 'link']);
+        $resolver->setAllowedValues('tag', ['link', 'button', 'input', 'submit', 'reset']);
+        $resolver->setAllowedValues('text', ['light', 'dark', '']);
+        $resolver->setAllowedValues('size', ['lg', 'sm', '']);
+
+        $resolver->setNormalizer('type', function(Options $options, $value) {
+            if ( $options['outline']) {
+                return 'btn-outline-' . $value;    
+            }
+
+            return 'btn-' . $value;
         });
+        
         $resolver->setNormalizer('text', function(Options $options, $value) {
             return 'text-' . $value;
         });
-        
 
+        $resolver->setNormalizer('size', function(Options $options, $value) {
+            return 'btn-' . $value;
+        });
+
+        $resolver->setNormalizer('disabled', function(Options $options, $value) {
+            return $value ? 'disabled' : '';
+       });
+        
         return $resolver->resolve($data);
     }
 }
